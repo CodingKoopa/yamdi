bbb13ba369cbbef8076a853b667c6c1536ed24c0
Koopa <codingkoopa@gmail.com>
2019-04-10 18:53:49 -0400

    Clean up Dockerfile, improve build logic.

--- Dockerfile
+++ Dockerfile
@@ -1,21 +1,29 @@
 # Set the base image to the official OpenJDK image. 8 is an LTS release, with Alpine Linux support.
 FROM openjdk:8-alpine
 
-# Install dependencies
-# TODO: Some of these can probably be reduced.
-RUN apk upgrade --update && \
-    apk add --update wget curl ca-certificates openssl bash git screen util-linux sudo shadow nss imagemagick && \
-    update-ca-certificates
+# Install the dependencies.
+# - bash    Bash, for running the Spigot startup script.
+# - git     Git, for BuildTools to clone the repositories.
+RUN apk upgrade --update --no-cache && \
+    apk add --update bash git && \
+    rm -rf /var/cache/apk/*
 
-#default directory for SPIGOT-server
+# Set the directory for the Spigot installation to be kept.
 ENV SPIGOT_DIRECTORY /opt/spigot
+# Set the directory for the command named pipe to be.
 ENV COMMAND_INPUT_FILE_PATH=/tmp/spigot-commmand-input
 
+# Add the Spigot launch Bash script to the image.
 ADD ./lib/scripts/spigot.sh /spigot.sh
+# Add the Spigot command running script to the image.
 ADD ./lib/scripts/spigot_cmd.sh /spigot_cmd.sh
 
+# Expose the Minecraft server port.
 EXPOSE 25565
+# Expose the Dynmap web port.
 EXPOSE 8123
+
+# Create a mount point for the Spigot installation directory.
 VOLUME ["/opt/spigot"]
 
 # Set the container entrypoint to the startup script.
--- lib/scripts/spigot.sh
+++ lib/scripts/spigot.sh
@@ -12,24 +12,29 @@ if [ -z "$SPIGOT_MEMORY_AMOUNT" ]; then
     SPIGOT_MEMORY_AMOUNT="1024M"
 fi
 
-# Force rebuild of spigot.jar if REV is latest.
-rm -f $SPIGOT_DIRECTORY/spigot-latest.jar
-
-# Only build a new spigot.jar if a jar for this REV does not already exist.
-if [ ! -f $SPIGOT_DIRECTORY/spigot-$REV.jar ]; then
-  echo "Building spigot jar file, be patient"
-  mkdir -p /tmp/buildSpigot
-  pushd /tmp/buildSpigot
+# Only build a new spigot.jar if manually enabled, or if a jar for this REV does not already exist.
+if [ "$FORCE_SPIGOT_REBUILD" = true ] || [ ! -f $SPIGOT_DIRECTORY/spigot-$REV.jar ]; then
+  echo "Building Spigot."
+  # Build in a temporary directory.
+  declare -r SPIGOT_BUILD_DIRECTORY=/tmp/spigot-build
+  mkdir -p "$SPIGOT_BUILD_DIRECTORY"
+  pushd "$SPIGOT_BUILD_DIRECTORY"
+  # Download the latest BuildTools JAR.
   wget https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
-  HOME=/tmp/buildSpigot java $JVM_OPTS -Xmx${BUILDTOOLS_MEMORY_AMOUNT} -Xms${BUILDTOOLS_MEMORY_AMOUNT} -jar BuildTools.jar --rev $REV
-  cp /tmp/buildSpigot/Spigot/Spigot-Server/target/spigot-*.jar $SPIGOT_DIRECTORY/spigot-$REV.jar
+  # Run BuildTools with the specified RAM, for the specified revision.
+  java $JVM_OPTS -Xmx${BUILDTOOLS_MEMORY_AMOUNT} -Xms${BUILDTOOLS_MEMORY_AMOUNT} -jar BuildTools.jar --rev $REV
+  # Copy the Spigot build to the Spigot directory.
+  cp spigot-*.jar "$SPIGOT_DIRECTORY/spigot-$REV.jar"
   popd
-  rm -rf /tmp/buildSpigot
+  # Remove the build files to preserve space.
+  rm -rf "$SPIGOT_BUILD_DIRECTORY"
   mkdir -p $SPIGOT_DIRECTORY/plugins
 fi
 
-# Select the spigot.jar for this particular rev.
-rm -f $SPIGOT_DIRECTORY/spigot.jar && ln -s $SPIGOT_DIRECTORY/spigot-$REV.jar $SPIGOT_DIRECTORY/spigot.jar
+# Remove any preexisting build.
+rm -f $SPIGOT_DIRECTORY/spigot.jar
+# Select the specified revision.
+ln -s $SPIGOT_DIRECTORY/spigot-$REV.jar $SPIGOT_DIRECTORY/spigot.jar
 
 rm -f "$COMMAND_INPUT_FILE_PATH"
 mkfifo -m700 "$COMMAND_INPUT_FILE_PATH"
