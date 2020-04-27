951981900acc85d145b9f6f5978594fcd8771f3a
Koopa <codingkoopa@gmail.com>
2020-04-27 00:51:48 -0400

    Experiment with building both Dockerfiles.

--- CI/Build/Docker.sh
+++ CI/Build/Docker.sh
@@ -11,11 +11,15 @@ echo "Pulling latest image \"$TAG_LONG_LATEST\" to use as cache."
 # Pull the last built Docker image to use as a cache. This may fail.
 docker pull "$TAG_LONG_LATEST" || true
 echo "Building Docker image for architecture \"$TARGET_ARCH\", as \"$TAG_LONG\"."
-# Build a Docker image for the target architecture.
-docker build --build-arg TARGET_ARCH="$TARGET_ARCH" --cache-from "$TAG_LONG_LATEST" -t "$TAG_LONG" .
+# Build Docker images for the target architecture.
+docker build --build-arg TARGET_ARCH="$TARGET_ARCH" --cache-from "$TAG_LONG_LATEST" \
+  -t "$TAG_LONG-hotspot" -f Dockerfile.openjdk.hotspot .
+docker build --build-arg TARGET_ARCH="$TARGET_ARCH" --cache-from "$TAG_LONG_LATEST" \
+  -t "$TAG_LONG-openj9" -f Dockerfile.openjdk.openj9 .
 echo "Making artifact directory."
 # Make a directory to save the built image to, as an artifact.
 mkdir Build
-echo "Saving Docker image as artifact."
+echo "Saving Docker images as artifact."
 # Save the built Docker image to the build directory.
-docker save --output "Build/$(CI/Common/GetTagShort.sh).tar" "$TAG_LONG"
+docker save --output Build/"$(CI/Common/GetTagShort.sh)"-hotspot.tar "$TAG_LONG"
+docker save --output Build/"$(CI/Common/GetTagShort.sh)"-openj9.tar "$TAG_LONG"
--- CI/Deploy/Docker.sh
+++ CI/Deploy/Docker.sh
@@ -12,23 +12,29 @@ CI/Common/Login.sh
 
 echo "Loading Docker image artifact."
 # Load the built Docker image from the build directory.
-docker load --input Build/"$(CI/Common/GetTagShort.sh)".tar
+docker load --input Build/"$(CI/Common/GetTagShort.sh)"-hotspot.tar
+docker load --input Build/"$(CI/Common/GetTagShort.sh)"-openj9.tar
 echo "Pushing Docker image for architecture \"$TARGET_ARCH\" to \"$TAG_LONG\"."
 # Push the Docker image to the registry.
-docker push "$TAG_LONG"
+docker push "$TAG_LONG-hotspot"
+docker push "$TAG_LONG-openj9"
 echo "Tagging Docker image \"$TAG_LONG\" as \"$TAG_LONG_LATEST\"."
 # Tag the Docker image as the latest image.
-docker tag "$TAG_LONG" "$TAG_LONG_LATEST"
+docker tag "$TAG_LONG-hotspot" "$TAG_LONG_LATEST-hotspot"
+docker tag "$TAG_LONG-openj9" "$TAG_LONG_LATEST-openj9"
 echo "Pushing Docker image for architecture \"$TARGET_ARCH\" to \"$TAG_LONG_LATEST\" (Latest)."
 # Push the latest tag to the registry.
-docker push "$TAG_LONG_LATEST"
+docker push "$TAG_LONG_LATEST-hotspot"
+docker push "$TAG_LONG_LATEST-openj9"
 # If a Git tag is present.
 if [ -n "$CI_COMMIT_TAG" ]; then
   TAG_LONG_STABLE=$CI_REGISTRY_IMAGE/$TARGET_ARCH:stable
   echo "Tagging Docker image \"$TAG_LONG\" as \"$TAG_LONG_STABLE\"."
   # Tag the Docker image as the latest stable image.
-  docker tag "$TAG_LONG" "$TAG_LONG_STABLE"
+  docker tag "$TAG_LONG-hotspot" "$TAG_LONG_STABLE-hotspot"
+  docker tag "$TAG_LONG-openj9" "$TAG_LONG_STABLE-openj9"
   echo "Pushing Docker image for architecture \"$TARGET_ARCH\" to \"$TAG_LONG_LATEST\" (Stable)."
   # Push the stable tag to the registry.
-  docker push "$TAG_LONG_STABLE"
+  docker push "$TAG_LONG_STABLE-hotspot"
+  docker push "$TAG_LONG_STABLE-openj9"
 fi
